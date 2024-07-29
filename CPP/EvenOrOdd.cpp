e1d447e270b066c847a7e48d4b9e490be317ff7d
Lloyd <Lloydabonds@gmail.com>
2024-07-29 09:20:44 -0400

    test 1

--- CPP/EvenOrOdd.cpp
+++ CPP/EvenOrOdd.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-#include <ostream>
-#include <string>
+using namespace std;
 
-std::string even_or_odd(int number){
+string even_or_odd(int number){
     if (number % 2 == 0) {
-        
+        cout << number << " is EVEN"
     }
+    else {
+        cout << number << " is ODD"
+    }
+}; 
+
+int main(){
+    int number; 
+    cout << "Enter a number: "; 
+    cin >> number;
+    cout << even_or_odd(number) << endl;
+    system("pause");
 }
